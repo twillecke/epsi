295b619f5b15c136503589d2eb45cf9a5f741339
twillecke <thiago.willecke@ino-games.com>
2024-03-24 18:31:07 -0300

    test: add test for user signup usecase

---
+++ src/application/usecase/UserSignUp.ts
@@ -0,0 +1,48 @@
+import PsychologistRepository from "../../domain/repository/PsychologistRepository";
+import UserCredentials from "../../../src/domain/entity/UserCredentials";
+import PsychologistProfile from "../../../src/domain/entity/PsychologistProfile";
+import UserCredentialsRepository from "../../../src/domain/repository/UserCredentialsRepository";
+import CypherService from "../../../src/infra/service/CypherService";
+
+export default class UserSignUp {
+  psychologistRepository: any;
+  userCredentialsRepository: any;
+  constructor(psychologistRepository: PsychologistRepository, userCredentialsRepository: UserCredentialsRepository) {
+    this.psychologistRepository = psychologistRepository;
+    this.userCredentialsRepository = userCredentialsRepository;
+  }
+
+  async execute(input: any) {
+    console.log("user sign-up", input);
+
+    const existingUserCpf = await this.psychologistRepository.getByCpf(input.cpf);
+    const existingUserEmail = await this.userCredentialsRepository.getByEmailAddress(input.emailAddress);
+    const existingUsername = await this.userCredentialsRepository.getByUsername(input.username);
+
+    if (existingUserCpf) throw new Error("Cpf already exists");
+    if (existingUserEmail) throw new Error("Email already exists");
+    if (existingUsername) throw new Error("Username already exists");
+
+    const hashedPassword = await CypherService.encrypt(input.password);
+
+    const userCredentialsOutput = await UserCredentials.create(input.username, input.email_address, hashedPassword, input.role);
+    const psychologistProfileOutput = await PsychologistProfile.create(userCredentialsOutput.getUserId(), input.name, input.birthdate, input.cpf, input.phone, input.city, input.province, input.address);
+
+    await this.userCredentialsRepository.save(userCredentialsOutput);
+    await this.psychologistRepository.save(psychologistProfileOutput);
+
+    return {
+      userId: userCredentialsOutput.getUserId(),
+      username: userCredentialsOutput.getUsername(),
+      email_address: userCredentialsOutput.getEmailAddress(),
+      role: userCredentialsOutput.getRole(),
+      name: psychologistProfileOutput.getName(),
+      birthdate: psychologistProfileOutput.getBirthdate(),
+      cpf: psychologistProfileOutput.getCpf(),
+      phone: psychologistProfileOutput.getPhone(),
+      city: psychologistProfileOutput.getCity(),
+      province: psychologistProfileOutput.getProvince(),
+      address: psychologistProfileOutput.getAddress()
+    }
+  }
+}
