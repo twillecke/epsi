b92fa335bea538decb82e49ff83f3f8176b99262
twillecke <thiago.willecke@ino-games.com>
2024-03-24 18:33:05 -0300

    feat: add types for repository raw data types

---
+++ src/infra/repository/PsychologistProfileData.ts
@@ -0,0 +1,10 @@
+export type PsychologistProfileData = {
+  userId: string;
+  name: string;
+  birthdate: string;
+  cpf: string;
+  phone: string;
+  city: string;
+  province: string;
+  address: string;
+}
---
+++ src/infra/repository/UserCredentialsData.ts
@@ -0,0 +1,8 @@
+export type UserCredentialsData = {
+  userId: string;
+  created_at: string;
+  username: string;
+  password: string;
+  email_address: string;
+  role: string;
+}
