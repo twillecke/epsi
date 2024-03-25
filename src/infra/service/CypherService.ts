9b5407a272998efad59093b27edf8556adfa70b6
twillecke <thiago.willecke@ino-games.com>
2024-03-25 09:09:43 -0300

    feat: add method to compare plain and hashed password in cypher service; read salt rounds amount from environment variables

--- src/infra/service/CypherService.ts
+++ src/infra/service/CypherService.ts
@@ -1,14 +1,16 @@
 const bcrypt = require("bcrypt");
+import * as dotenv from 'dotenv';
+
 export default class CypherService {
   constructor(readonly bcrypt: any) { }
 
   static async encrypt(password: string): Promise<string> {
-    const saltRounds = 10;
+    dotenv.config();
+    const saltRounds = process.env.ENCRYPTION_SALT_ROUNDS || 10;
     return await bcrypt.hash(password, saltRounds);
   }
 
-  static async decrypt(password: string): Promise<string> {
-    const saltRounds = 10;
-    return await bcrypt.hash(password, saltRounds);
+  static async compare(plainPassword: string, hashedPassword: string): Promise<string> {
+    return await bcrypt.compare(plainPassword, hashedPassword);
   }
 }
