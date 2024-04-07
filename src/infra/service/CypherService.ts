239edaa58a83793ebc9be7a4205b4f1f2ea15334
twillecke <thiago.willecke@ino-games.com>
2024-04-07 13:42:40 -0300

    fix: get salt rounds from .env

--- src/infra/service/CypherService.ts
+++ src/infra/service/CypherService.ts
@@ -6,11 +6,11 @@ export default class CypherService {
 
   static async encrypt(password: string): Promise<string> {
     dotenv.config();
-    const saltRounds = process.env.ENCRYPTION_SALT_ROUNDS || 10;
+    const saltRounds = parseInt(process.env.ENCRYPTION_SALT_ROUNDS as string) || 10;
     return await bcrypt.hash(password, saltRounds);
   }
 
-  static async compare(plainPassword: string, hashedPassword: string): Promise<string> {
+  static async compare(plainPassword: string, hashedPassword: string): Promise<boolean> {
     return await bcrypt.compare(plainPassword, hashedPassword);
   }
 }
