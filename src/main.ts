94b20e08ce8c747f3c93143dbed1818693b3cdc7
twillecke <thiago.willecke@ino-games.com>
2024-05-01 16:11:02 -0300

    feat: add login and user sign up api endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,45 @@
 import * as dotenv from 'dotenv';
 import cors from "cors";
 import express from "express";
+import UserSignUp from './application/usecase/UserSignUp';
+import { PgPromiseAdapter } from './infra/database/DatabaseConnection';
+import UserCredentialsRepositoryDatabase from './infra/repository/UserCredentialsRepositoryDatabase';
+import PsychologistRepositoryDatabase from './infra/repository/PsychologistRepositoryDatabase';
+import UserSignIn from './application/usecase/UserSignIn';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('');
+app.post('/api/login', async (req, res) => {
+  const input = req.body;
+  const connection = new PgPromiseAdapter();
+  const userCredentialsRepositoryDatabase = new UserCredentialsRepositoryDatabase(connection);
+  const userSignIn: UserSignIn = new UserSignIn(userCredentialsRepositoryDatabase);
+  try {
+    const output = await userSignIn.execute(input);
+    console.log(output);
+  }
+  catch (e) {
+    console.log(e);
+  }
+  connection.close();
 });
 
-app.listen(process.env.SERVER_PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+app.post('/api/users', async (req, res) => {
+  const input = req.body;
+  const connection = new PgPromiseAdapter();
+  const userCredentialsRepositoryDatabase = new UserCredentialsRepositoryDatabase(connection);
+  const psychologistRepositoryDatabase = new PsychologistRepositoryDatabase(connection);
+  const userSignUp: UserSignUp = new UserSignUp(psychologistRepositoryDatabase, userCredentialsRepositoryDatabase);
+  await userSignUp.execute(input);
+  connection.close();
+})
+
+app.listen(process.env.HTTP_SERVER_PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.HTTP_SERVER_PORT}`);
 });
+
+
+
